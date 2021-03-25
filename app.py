d6e88030343463218cf75ccffc74254469dd1a5c
Aaditya Dubey <52327349+AadityaDubey@users.noreply.github.com>
2021-03-25 11:45:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import pickle
 import pandas as pd
 
 # Use pickle to load in the pre-trained model
-with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
-    model = pickle.load(f)
-
+with open(f'model/final_model.pkl', 'rb') as f:
+     model = pickle.load(f)
 # Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -27,10 +26,11 @@ def main():
                                        columns=['temperature', 'humidity', 'windspeed'],
                                        dtype=float,
                                        index=['input'])
+		#input_variables = np.array([[temperature, humidity, windspeed]])
 
         # Get the model's prediction
-        prediction = model.predict(input_variables)[0]
-    
+        prediction = model.predict(input_variables.values)[0]
+        #prediction = model.predict(input_variables)
         # Render the form again, but add in the prediction and remind user
         # of the values they input before
         return flask.render_template('main.html',
